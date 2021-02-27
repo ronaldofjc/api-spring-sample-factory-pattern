5fdb70d72061acd2626493ee340f103752b076f7
Ronaldo Feitosa <rfeitosa@ciandt.com>
2021-02-27 15:05:41 -0300

    Initial commit

---
+++ src/main/java/br/com/sample/factory/pattern/SampleFactoryPatternApplication.java
@@ -0,0 +1,13 @@
+package br.com.sample.factory.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleFactoryPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleFactoryPatternApplication.class, args);
+	}
+
+}
